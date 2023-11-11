4a88c9d20d88339eab616b015f35577342cc4353
志村啓太 <keitashimura2023@gmail.com>
2023-11-12 04:39:28 +0900

    feat: 環境変数の設定

---
+++ src/database/config.go
@@ -0,0 +1,14 @@
+package database
+
+import (
+	"os"
+)
+
+var (
+	DBUsername   = os.Getenv("DB_USERNAME")   // データベースのユーザー名
+	DBPassword   = os.Getenv("DB_PASSWORD")   // データベースのパスワード
+	DBHost       = os.Getenv("DB_HOST")       // データベースサーバーのホスト名またはIPアドレス
+	DBPort       = os.Getenv("DB_PORT")       // データベースサーバーのポート
+	DBName       = os.Getenv("DB_NAME")       // データベース名
+	DBParameters = os.Getenv("DB_PARAMETERS") // データベース接続パラメーター
+)
