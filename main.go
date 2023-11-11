bc050746dea8b7dd243422a4432d57d2a8d4871c
志村啓太 <keitashimura2023@gmail.com>
2023-11-12 03:06:56 +0900

    feat: DBへの接続

--- main.go
+++ main.go
@@ -4,14 +4,27 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+	// Open the connection to the database
+	_, err := gorm.Open(mysql.Open("keita:@Keita8001@tcp(db:3306)/sadapp?parseTime=true"), &gorm.Config{})
+
+	// Check for errors in opening the database connection
+	if err != nil {
+		panic("データベースに接続できませんでした。") // "Could not connect to the database."
+	}
+
+	// Create a new Fiber app
 	app := fiber.New()
 
+	// Define a route
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
 
+	// Start the server
 	log.Fatal(app.Listen(":8002"))
 }
