204aeac66b9ccb6c30cba7013aa7e6b745baced7
志村啓太 <keitashimura2023@gmail.com>
2023-11-25 04:34:01 +0900

    feat: 不要なカラム削除

--- src/models/EventParticipant.go
+++ src/models/EventParticipant.go
@@ -5,7 +5,6 @@ import "time"
 type EventParticipant struct {
 	EventId   uint      `json:"event_id" gorm:"primaryKey"`
 	UserId    uint      `json:"user_id" gorm:"primaryKey"`
-	Status    string    `json:"status"` // 例: "confirmed", "pending", etc.
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
