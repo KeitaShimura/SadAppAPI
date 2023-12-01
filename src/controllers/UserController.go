42c2e7a43a086b8cd6c4e0aa2ba24d69a7afd2dc
志村啓太 <keitashimura2023@gmail.com>
2023-12-01 18:51:55 +0900

    feat: Userのバリデーション追加

--- src/controllers/UserController.go
+++ src/controllers/UserController.go
@@ -57,42 +57,75 @@ func GetUser(c *fiber.Ctx) error {
 
 // UpdateUser 関数は、ユーザー情報を更新するための関数です。
 func UpdateUser(c *fiber.Ctx) error {
-	// リクエストボディからデータを読み込むためのマップを定義
-	var data map[string]string
+    // リクエストボディからデータを読み込むためのマップを定義
+    var data map[string]string
 
-	// リクエストボディの解析。エラーがあれば返す
-	if err := c.BodyParser(&data); err != nil {
-		return err
-	}
+    // リクエストボディの解析。エラーがあれば返す
+    if err := c.BodyParser(&data); err != nil {
+        return err
+    }
 
-	// ミドルウェアを通じて現在のユーザーIDを取得
-	id, _ := middlewares.GetUserId(c)
+    // ユーザーデータのバリデーション
+    if len(data["name"]) < 1 || len(data["name"]) > 255 {
+        return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+            "error": "ユーザー名は1文字以上255文字以下である必要があります。",
+        })
+    }
 
-	// 更新するユーザーのデータを取得するためのUserモデルのインスタンスを作成
-	var user models.User
+    if len(data["email"]) < 1 || len(data["email"]) > 255 {
+        return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+            "error": "メールアドレスは1文字以上255文字以下である必要があります。",
+        })
+    }
 
-	// データベースからIDに基づいてユーザー情報を取得
-	result := database.DB.Where("id = ?", id).First(&user)
-	if result.Error != nil {
-		// エラーがあれば、そのエラーを返す
-		return result.Error
-	}
+    if len(data["bio"]) > 1000 {
+        return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+            "error": "自己紹介文は1000文字以下である必要があります。",
+        })
+    }
 
-	// ユーザーデータを更新
-	user.Name = data["name"]
-	user.Email = data["email"]
-	user.Bio = data["bio"]
-	user.Icon = data["icon"]
-	user.Banner = data["banner"]
-	user.Location = data["location"]
-	user.WebSite = data["website"]
-	user.BirthDate = data["birth_date"]
+    if len(data["website"]) > 255 {
+        return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+            "error": "ウェブサイトのURLは255文字以下である必要があります。",
+        })
+    }
 
-	// 更新されたデータをデータベースに保存
-	database.DB.Save(&user)
+    if len(data["birth_date"]) > 255 {
+        return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+            "error": "生年月日は255文字以下である必要があります。",
+        })
+    }
 
-	// 更新されたユーザー情報をJSON形式でレスポンスとして返す
-	return c.JSON(user)
+    // ミドルウェアを通じて現在のユーザーIDを取得
+    id, _ := middlewares.GetUserId(c)
+
+    // 更新するユーザーのデータを取得するためのUserモデルのインスタンスを作成
+    var user models.User
+
+    // データベースからIDに基づいてユーザー情報を取得
+    result := database.DB.Where("id = ?", id).First(&user)
+    if result.Error != nil {
+        // エラーがあれば、そのエラーを返す
+        return result.Error
+    }
+
+    // リクエストデータから更新対象のユーザーデータを一時変数に格納
+    updateData := map[string]interface{}{
+        "Name":      data["name"],
+        "Email":     data["email"],
+        "Bio":       data["bio"],
+        "Icon":      data["icon"],
+        "Banner":    data["banner"],
+        "Location":  data["location"],
+        "WebSite":   data["website"],
+        "BirthDate": data["birth_date"],
+    }
+
+    // 一時変数のデータをユーザーデータに反映
+    database.DB.Model(&user).Updates(updateData)
+
+    // 更新されたユーザー情報をJSON形式でレスポンスとして返す
+    return c.JSON(user)
 }
 
 // UpdatePassword 関数は、ユーザーのパスワードを更新するための関数です。
@@ -110,6 +143,14 @@ func UpdatePassword(c *fiber.Ctx) error {
 		c.Status(400)
 		return c.JSON(fiber.Map{
 			"message": "パスワードが一致しません。", // エラーメッセージをJSONで返す
+		})
+	}
+
+	// パスワードのバリデーション：8文字以上
+	if len(data["password"]) < 8 {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "パスワードは8文字以上である必要があります。",
 		})
 	}
 
