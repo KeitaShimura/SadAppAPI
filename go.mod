1e1967f7fe8e17ae879917be311bb2eb2a8304eb
志村啓太 <keitashimura2023@gmail.com>
2023-12-24 01:04:23 +0900

    feat: 一時保存/不要なファイル削除

--- go.mod
+++ go.mod
@@ -10,6 +10,10 @@ require (
 	github.com/gofiber/fiber/v2 v2.50.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/google/uuid v1.3.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
+	github.com/jackc/pgx/v5 v5.5.1 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
@@ -24,9 +28,12 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.50.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/crypto v0.15.0 // indirect
-	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/sync v0.5.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/mysql v1.5.2 // indirect
+	gorm.io/driver/postgres v1.5.4 // indirect
 	gorm.io/gorm v1.25.5 // indirect
 )
--- src/database/db.go
+++ src/database/db.go
@@ -4,59 +4,57 @@ import (
 	"SadApp/src/models"
 	"fmt"
 	"log"
+	"os"
 
-	"gorm.io/driver/mysql"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
+
+// 環境変数をロードする関数
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+}
 
 // DB は、データベース接続を保持するためのグローバル変数です。
 var DB *gorm.DB
 
 // Connect はデータベースへの接続を確立する関数です。
 func Connect() {
-	var err error
-	// 環境変数が設定されていない場合、デフォルトの情報を使用
-	if DBUsername == "" {
-		DBUsername = "keita"
-	}
-	if DBPassword == "" {
-		DBPassword = "@Keita8001"
-	}
-	if DBHost == "" {
-		DBHost = "localhost"
-	}
-	if DBPort == "" {
-		DBPort = "3306"
-	}
-	if DBName == "" {
-		DBName = "sadapp"
-	}
-	if DBParameters == "" {
-		DBParameters = "charset=utf8mb4&parseTime=True&loc=Local"
-	}
+	loadEnv()
 
-	// dsn（データソース名）を組み立てます。
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?%s", DBUsername, DBPassword, DBHost, DBPort, DBName, DBParameters)
+	DBUsername := os.Getenv("DB_USERNAME")
+	DBPassword := os.Getenv("DB_PASSWORD")
+	DBHost := os.Getenv("DB_HOST")
+	DBPort := os.Getenv("DB_PORT")
+	DBName := os.Getenv("DB_NAME")
+	DBParameters := os.Getenv("DB_PARAMETERS")
+
+	var err error
+
+	// PostgreSQL用のDSNを組み立てます。
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s %s", DBHost, DBUsername, DBPassword, DBName, DBPort, DBParameters)
 
 	// データベースに接続を開きます。
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode((logger.Silent)),
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
 	})
 
 	// データベース接続時のエラーをチェックします。
 	if err != nil {
-		// 接続に失敗した場合、プログラムをパニック状態にします。
-		panic("データベースに接続できませんでした。") // "Could not connect to the database."
+		panic("データベースに接続できませんでした。")
 	}
 }
 
 // AutoMigrate はデータベースのスキーマを自動的にマイグレーション（更新）する関数です。
 func AutoMigrate() {
 	// Userモデルを使用して、データベースのスキーマを自動的にマイグレーションします。
-	err := DB.AutoMigrate(models.User{}, models.Follow{}, models.Post{}, models.PostComment{}, models.PostLike{}, models.Event{}, models.EventComment{}, models.EventLike{}, models.EventParticipant{})
+	err := DB.AutoMigrate(&models.User{}, &models.Follow{}, &models.Post{}, &models.PostComment{}, &models.PostLike{}, &models.Event{}, &models.EventComment{}, &models.EventLike{}, &models.EventParticipant{})
 	if err != nil {
-		// マイグレーションに失敗した場合、エラーをログに記録し、プログラムを終了します。
 		log.Fatalf("データベースのマイグレーションに失敗しました: %v", err)
 	}
 }
