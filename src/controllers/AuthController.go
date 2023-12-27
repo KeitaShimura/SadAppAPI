51bb40a3e608a8f9eaf92b0650bdcc44fc823c67
志村啓太 <keitashimura2023@gmail.com>
2023-12-28 02:14:53 +0900

    feat: setTokenCookieの変更

--- src/controllers/AuthController.go
+++ src/controllers/AuthController.go
@@ -200,6 +200,7 @@ func setTokenCookie(c *fiber.Ctx, token string) {
 		Value:    token,
 		Expires:  time.Now().Add(time.Hour * 24),
 		HTTPOnly: true,
+		SameSite: "Strict",
 	}
 	c.Cookie(&cookie)
 }
